06f6e95c733a257ef83c554dfbfb03b7cf841085
Guichard Manigat <gukardm@gmail.com>
2023-09-20 11:12:22 -0400

    new

--- CPTR420_9_20/CPTR420_GM.r
+++ CPTR420_9_20/CPTR420_GM.r
@@ -1,13 +1,14 @@
 # 0/20/23
 # Guichard Manigat
 
-# Laods library
+# Loads library
 library(dplyr)
 
 
-library(magrittr)
 # Choses file to read, keeps header, and converts blank variables into NA strings
-yourFileName <- read.csv(file.choose(), header=T, na.strings = '')
+yourFileName <- read.csv('CPTR420_9_20/September_19_Data.csv', header=T, na.strings = '')
+
+#yourFileName <- read.csv(file.choose(), header=T, na.strings = '')
 
 
 # Randomly select 3/5 of rows in yourFileName into GM_sampleDF
@@ -25,3 +26,19 @@ GM_sampleDF <- GM_sampleDF %>%
 #GM_sampleDF['WHEELS_OFF']
 
 # Change the name of column CARRIER to Airline
+GM_sampleDF <- GM_sampleDF %>%
+    rename('Airline' = 'CARRIER')
+
+#colnames(GM_sampleDF)
+
+#Remove leading/trailing spaces in DEST_STATE and ORIGIN_STATE
+GM_sampleDF <- GM_sampleDF %>%
+    mutate(
+    GM_sampleDF['DEST_STATE'] <- trimws(GM_sampleDF['DEST_STATE']),
+    GM_sampleDF['ORIGIN_STATE'] <- trimws(GM_sampleDF['ORIGIN_STATE'])
+    )
+
+# Show all variables for Southwest (WN) flights
+GM_sampleDF <- GM_sampleDF %>%
+    filter(CARRIER == 'WN')
+
