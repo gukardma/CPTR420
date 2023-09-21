a5019a8a6f37ef662523c3806712ee422f1e2b56
Guichard <gukardm@gmail.com>
2023-09-20 23:27:37 -0400

    s

--- CPTR420_9_20/CPTR420_GM.r
+++ CPTR420_9_20/CPTR420_GM.r
@@ -1,23 +1,24 @@
-# 0/20/23
+# 9/20/23
 # Guichard Manigat
 
-# View(GM_sampleDF)
-
-install.packages(c("dplyr"))
+# If not installed, install the package
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+    install.packages("dplyr")
+}
 
 # Loads library
 library(dplyr)
 
+# 0 Check if file exists, otherwise choose, keeps header, and converts blank variables into NA strings
+if (file.exists("CPTR420_9_20/September_19_Data.csv")) {
+  df <- read.csv("CPTR420_9_20/September_19_Data.csv", header = TRUE, na.strings = "")
+} else {
+  df <- read.csv(file.choose(), header = TRUE, na.strings = "")
+}
 
-# 0 Choses file to read, keeps header, and converts blank variables into NA strings
-# Relative path
-# yourFileName <- read.csv("CPTR420_9_20/September_19_Data.csv", header = T, na.strings = "")
-
-yourFileName <- read.csv(file.choose(), header = T, na.strings = "")
-
-# 1 Randomly select 3/5 of rows in yourFileName into GM_sampleDF
-GM_sampleDF <- yourFileName %>%
-    sample_frac(3 / 5)
+# 1 Randomly select 3/5 of rows in df into GM_sampleDF
+GM_sampleDF <- df %>%
+    sample_frac(0.6)
 
 # 2 List all the column names
 colnames(GM_sampleDF)
@@ -26,63 +27,74 @@ colnames(GM_sampleDF)
 GM_sampleDF <- GM_sampleDF %>%
     select(-c("WHEELS_OFF", "WHEELS_ON"))
 
-# Check if exists (returns error)
-# GM_sampleDF['WHEELS_OFF']
-
 # 4 Change the name of column CARRIER to Airline
 GM_sampleDF <- GM_sampleDF %>%
     rename("Airline" = "CARRIER")
 
-# colnames(GM_sampleDF)
-
-# 5 Remove leading/trailing spaces in DEST_STATE and ORIGIN_STATE
-GM_sampleDF <- GM_sampleDF %>%
-    mutate(
-        GM_sampleDF["DEST_STATE"] <- trimws(GM_sampleDF["DEST_STATE"]),
-        GM_sampleDF["ORIGIN_STATE"] <- trimws(GM_sampleDF["ORIGIN_STATE"])
-    )
+# 5 Remove leading/trailing spaces in DEST_STATE and ORIGIN_STATE (Does not work?)
+# GM_sampleDF <- GM_sampleDF %>%
+#     mutate(
+#         GM_sampleDF["DEST_STATE"] <- trimws(GM_sampleDF["DEST_STATE"]),
+#         GM_sampleDF["ORIGIN_STATE"] <- trimws(GM_sampleDF["ORIGIN_STATE"])
+#     )
 
 # 6 Show all variables for Southwest (WN) flights
-GM_sampleDF_WN <- GM_sampleDF %>%
+QUES_6 <- GM_sampleDF %>%
     filter(Airline == "WN")
 
-
 # 7 Show flight date, carrier, origin city, destination city for all observations(i.e. rows)
-GM_sampleDF_Sel <- GM_sampleDF %>%
+QUES_7 <- GM_sampleDF %>%
     select(c("FL_DATE", "Airline", "ORIGIN_CITY", "DEST_CITY"))
 
 # 8 Show Flight Date, Origin, Destination, Airline, Cancelled, Cancellation Code of all cancelled flights
-GM_sampleDF_Cancled <- GM_sampleDF %>%
+QUES_8 <- GM_sampleDF %>%
     filter(CANCELLED == 1) %>%
     select(c("FL_DATE", "ORIGIN_CITY", "DEST_CITY", "Airline", "CANCELLED", "CANCELLATION_CODE", ))
 
 # 9 Show Flight Date, Carried, Origin, Destination, Origin State, Destination State, Scheduled Departure time, Scheduled Arrival Time for flights between New York Airports
-GM_sampleDF_New_York <- GM_sampleDF %>%
+QUES_9 <- GM_sampleDF %>%
     filter(ORIGIN_CITY == "New York" | DEST_CITY == "New York") %>%
     select(c("FL_DATE", "Airline", "ORIGIN_CITY", "DEST_CITY", "ORIGIN_STATE", "DEST_STATE", "DEP_TIME", "ARR_TIME"))
 
 # 10 Show Flight Date, Carrier, Day Name, Origin, Destination for Delta (DL) flights going to Miami on Friday
-GM_sampleDF_FD <- GM_sampleDF %>%
+QUES_10 <- GM_sampleDF %>%
     filter(DEST_CITY == "Miami" & DAY_NAME == "Friday") %>%
     select(c("FL_DATE", "Airline", "DAY_NAME", "ORIGIN_CITY", "DEST_CITY"))
 
 # 11 Show Flight Date, Carrier, Origin, Destination, Destination CIty, and Scheduled Departuer Time of flights between JFK or LGA and Florida
-GM_sampleDF_JFK <- GM_sampleDF %>%
+QUES_11 <- GM_sampleDF %>%
     filter(ORIGIN == "JFK" | ORIGIN == "LGA") %>%
     filter(DAY_NAME == "Friday") %>%
     select(c("FL_DATE", "Airline", "ORIGIN", "DEST", "DEST_CITY", "DEP_TIME"))
 
 # 12 Show flight date, carrier, origin, destination, and distance for all flights
 # with a distance more than 2200. SHow th results in order from tnearest to farthest
-GM_sampleDF_Dist <- GM_sampleDF %>%
+QUES_12 <- GM_sampleDF %>%
     filter(DISTANCE > 2200) %>%
     arrange(DISTANCE) %>%
     select(c("FL_DATE", "Airline", "ORIGIN", "DEST", "DISTANCE"))
-
-colnames(GM_sampleDF)
 
 # 13 Create a df called longFlghts and...
 longFlights <- GM_sampleDF %>%
     select(c("FL_DATE", "Airline", "ORIGIN", "DEST", "TAXI_OUT", "TAXI_IN", "AIR_TIME")) %>%
     mutate(Flight_Time = TAXI_OUT + TAXI_IN + AIR_TIME) %>%
-    filter(Flight_Time > 6)
+    filter(Flight_Time > 360)
+
+# 14 Show the average departure delay for each carrier for flights from jfk
+QUES_14 <- GM_sampleDF %>%
+    filter(ORIGIN == "JFK") %>%
+    group_by(Airline) %>%
+    summarise(AVG_DEP_DELAY = mean(DEP_DELAY, na.rm = TRUE))
+
+# 15 How many flight leave New York each day of the week
+QUES_15 <- GM_sampleDF %>%
+    filter(ORIGIN_CITY == "New York") %>%
+    group_by(DAY_NAME) %>%
+    summarise(NUM_FLIGHTS = n()) %>%
+    arrange(NUM_FLIGHTS)
+
+# 16 Which are the 10 most popular airlines (based on number of flights for that airline)
+QUES_16 <- GM_sampleDF %>%
+    group_by(Airline) %>%
+    summarise(NUM_FLIGHTS = n()) %>%
+    arrange(NUM_FLIGHTS)
