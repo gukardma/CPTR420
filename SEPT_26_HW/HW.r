576784a21c577815ee9ec35f78163c465be88bb1
Guichard Manigat <gukardm@gmail.com>
2023-10-03 13:47:59 -0400

    s

--- SEPT_26_HW/HW.r
+++ SEPT_26_HW/HW.r
@@ -1,9 +1,10 @@
-#Guichard Manigat
-#9-28-23
-
+# Guichard Manigat
+# 9-28-23
 install.packages("dplyr")
 install.packages("tidyr")
+install.packages("lubridate")
 
+library(lubridate)
 library(dplyr)
 library(tidyr)
 
@@ -19,7 +20,7 @@ summary(flights)
 
 # 3. spit the time_hour column into date and time. Do not remove the original
 # column
-flights <- separate(flights, col = time_hour, into = c("time", "hour"), sep = "T")
+flights <- separate(flights, col = time_hour, into = c("time", "hour"), sep = "T", remove = FALSE)
 
 # 4. for each df, decide what you think is the key variable(s) and check to see
 # whether there are any duplicates
@@ -30,39 +31,79 @@ colnames(airport) # name/faa
 colnames(flights) # tailnum
 colnames(weather) # time_hour
 
+
+# weatherKey <- weather %>%
+#     select(origin,time_hour)
+
+# sum(duplicated(weatherKey))
+
+
 # 5. Combine the flight day, month, year into a single field called flightDate
 # Similarly combine the year, month, day for the weather df into a single column
 # called obsDate
 
-flights <- unite(flights, col="flightDate", c("year","month","day"),sep="-", remove=TRUE)
+# flights <- flights %>%
+#     unite(col = "flightDate", c("year","month","day"),sep="-") %>%
+#     as.Date(weather$flightDate)
 
-#6. Create a new df called dailyWeather that will have the average temp, average
+
+# weather <- weather %>%
+#     unite(col = "obsDate", c("year","month","day"),sep="-") %>%
+#     as.Date(weather$obsDate)
+
+# using lubridate library
+
+flights$flDate <- make_date(flights$year, flights$month, flights$day)
+
+# 6. Create a new df called dailyWeather that will have the average temp,
+# average
 
 # Data cleaning
 weather <- weather %>%
-  mutate(
-    temp = as.numeric(temp),
-    wind_speed = as.numeric(wind_speed),
-    humid = as.numeric(humid)
-  )
+    mutate(
+        temp = as.numeric(temp),
+        wind_speed = as.numeric(wind_speed),
+        humid = as.numeric(humid)
+    )
 
-dailyWeather <- weather %>%
-  group_by(origin, day) %>%
-  summarize(
-    avg_temp = mean(temp, na.rm = TRUE),
-    avg_wind_speed = mean(wind_speed, na.rm = TRUE),
-    avg_humidity = mean(humid, na.rm = TRUE)
-  )
+daily_weather <- weather %>%
+    group_by(origin, day) %>%
+    summarize(
+        avg_temp = mean(temp, na.rm = TRUE),
+        avg_wind_speed = mean(wind_speed, na.rm = TRUE),
+        avg_humidity = mean(humid, na.rm = TRUE)
+    )
 
-#7. Create a new df called full_flights. This should have (flight, origin,
-#desHnaHon, … all the data from flights as well as the appropriate carrier
-#name, average temperature, average wind speed and average humidity
+# 7. Create a new df called full_flights. This should have (flight, origin,
+# desHnaHon, … all the data from flights as well as the appropriate carrier
+# name, average temperature, average wind speed and average humidity
 
-full_flights <- flights %>% left_join(dailyWeather, by = "origin", relationship = "many-to-many")
-head(full_flights)
+full_flights <- flights %>% left_join(dailyWeather, by = "day", relationship = "many-to-many")
 
-
-#8. Is there a relationship between the age of a plane and its delays?
+# 8. Is there a relationship between the age of a plane and its delays?
 flight_info <- flights %>% inner_join(airplanes, by = "tailnum")
+
 relation_plane <- flight_info %>%
-    group_by(dep_delay, year)
+    group_by(dep_delay, year) %>%
+    select(dep_delay, carrier, flight, tailnum, origin, year, day) %>%
+    summarize(
+        sort(decreasing = FALSE)
+    )
+
+# dont do 9
+# dont do 11
+
+# 10. Filter flights to only show flights with planes that have flown at
+# least 100 flights
+active_planes <- flights %>%
+    select(flight, tailnum) %>%
+    group_by(tailnum) %>%
+    summarize(numFlights = n()) %>%
+    filter(numFlights > 100)
+
+# 12. Find the 48 hours (over the course of the whole year) that have the
+# worst delays
+delayed_flights <- flights %>%
+    select(dep_delay,time_hour) %>%
+    filter(dep_delay > 0) %>%
+    group_by(time)
