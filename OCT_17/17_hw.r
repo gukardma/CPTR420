ca7950ae2d7d4a9fdae5920f212f0e91e312d099
Guichard <gukardm@gmail.com>
2023-10-22 13:27:04 -0400

    s

--- OCT_17/17_hw.r
+++ OCT_17/17_hw.r
@@ -2,61 +2,62 @@ library("lubridate")
 library("dplyr")
 library("tidyr")
 
+# 0. Setup
 lakers <- lubridate :: lakers
-
-# Date manipulation
-lakers$date <- as.Date(as.character(lakers$date), format = "%Y%m%d")
-
-lakers <- lakers %>%
-    separate(
-        col = "date",
-        c("Year", "Month", "Day"),
-        sep = "-",
-        remove = FALSE,
-    )
+lakers$time <- hm(lakers$time)
+lakers$date <- ymd(lakers$date)
 
 # 1. Who are the players on the team?
-players <- unique(lakers$player[!is.na(lakers$player)])
+Ques_1 <- unique(lakers$player[!is.na(lakers$player)])
 
 # 2. How many games are played (Home and Away)?
 length(unique(lakers$date[!is.na(lakers$date)]))
 
 # 3. For each game, who was the opposing team for each game? (Show date and team)
-game <- lakers %>%
+Ques_3 <- lakers %>%
   select(date, opponent) %>%
   distinct(date, .keep_all = TRUE) %>%
   filter(!is.na(date))
 
 # 4. How long did the season last (number of weeks)?
-difftime(game$date[78], game$date[1], units = "weeks")
+difftime(max(lakers$date), min(lakers$date), units = "weeks")
 
-# Calculate the difference in days
-days <- game$date[78] - game$date[1]
-
-# Convert days to weeks
-
-
-# 5. How long ago did the season end (number of months)? ?????
-days <- ymd(game$date[78])
-months_difference <- interval(specific_date, Sys.Date()) %/% months(1)
-
-
+# 5. How long ago did the season end (number of months)?
+Ques_5 <- as.period(interval(max(lakers$date), Sys.Date()))$year * 12 +
+  as.period(interval(max(lakers$date), Sys.Date()))$month
 
 # 6. The MVP for each game is the player who made the most shots. Who is the MVP for each 
 # game?
-
+Ques_6 <- lakers %>%
+  group_by(date) %>%
+  summarise(MVP = player[which.max(points)])
 
 # 7. Who was the MVP for the season?
-
+Ques_7 <- lakers %>%
+  group_by(player) %>%
+  summarise(totalPoints = sum(points)) %>%
+  arrange(desc(totalPoints)) %>%
+  head(1)
 
 # 8. What was the average time between shots attempted for each game?
-
+Ques_8 <- lakers %>%
+  group_by(date) %>%
+  arrange(time) %>%
+  mutate(diff = time - lag(time)) %>%
+  summarise(avgTime = mean(diff, na.rm = TRUE))
 
 # 9. What is the longest time between shots made for each game?
-
+Ques_9 <- lakers %>%
+  group_by(game) %>%
+  arrange(time) %>%
+  mutate(diff = time - lag(time)) %>%
+  summarise(timeShots = max(time_diff, na.rm = TRUE))
 
 # 10. Approximately how long did each game last?
-
+Ques_10 <- lakers %>%
+  group_by(game) %>%
+  arrange(time) %>%
+  summarise(duration = max(time) - min(time))
 
 # 11. What was the most popular game day. Show the number of games played on each day of the 
 # week?
@@ -66,4 +67,3 @@ months_difference <- interval(specific_date, Sys.Date()) %/% months(1)
 
 
 # 13. Were any games played on Christmas day or New Year’s Day, Thanksgiving Day?
-
